353a3dcdcb8a9b17da175e934792ae3a75209da5
nickthedude <nickthedude@me.com>
2016-10-28 06:38:34 -0700

    Initial commit. Removed Storyboards as requested. Replaced Storyboards with xib file and re-wired. Added Google maps pod. App is showing a simple map on screen. Need to read API key in from config file.

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'iCarsCodingTest' do
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+end
--- iCarsCodingTest/ViewController.swift
+++ iCarsCodingTest/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        GMSServices.provideAPIKey("AIzaSyB674PRDkSuHNmerNaIE4qXqDiM_UKGhxc")
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
