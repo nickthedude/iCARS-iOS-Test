b548ea3bc91dcbcf2dd5adccfd9d88ee4e42163b
nickthedude <nickthedude@me.com>
2016-10-29 14:16:08 -0700

    Added documentation to some of the methods in the MapViewController class

--- iCarsCodingTest/AppDelegate.swift
+++ iCarsCodingTest/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.black
         window?.makeKeyAndVisible()
-        let navController = UINavigationController.init(rootViewController: ViewController())
+        let navController = UINavigationController.init(rootViewController: MapViewController())
         // Setup our initialViewController
         window?.rootViewController = navController
         return true
--- iCarsCodingTest/MapViewController.swift
+++ iCarsCodingTest/MapViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 import GoogleMaps
 import CoreLocation
 
-class ViewController: UIViewController , CLLocationManagerDelegate {
+class MapViewController: UIViewController , CLLocationManagerDelegate {
 
+    /// CoreLocation CLLocation manager member variable
     private var _locationManager  = CLLocationManager()
+    
+    /// Google Maps camera object member variable used to manipulate the viewing bounds of a GMSMapView object
     private var _camera = GMSCameraPosition.camera(withLatitude: 0.0, longitude: 0.0, zoom: 6.0)
 
     
@@ -35,6 +38,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
 
     }
     
+    /// retrieves the configuration dictionary from the main app bundle. The caller of this function is responsible for extracting individual values from the returned Dictionary. If there is no config dictionary an optional containing a nil value is returned.
     func getConfigDictionary() -> [String : AnyObject]? {
         let path = Bundle.main.path(forResource: "configuration", ofType: "plist")
         if let dict = NSDictionary(contentsOfFile: path!) as? [String : AnyObject] {
@@ -45,6 +49,9 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
         }
     }
     
+    ///Convienence method for updating the _camera's viewable area based on the latitude and longitude parameters
+    /// - Parameter latitude: a latitude in Double format used to orient the _camera along a vertical axis
+    /// - Parameter longitude: a longitude in Double format used to orient the _camera along a horizontal axis
     func updateMapViewToLocation(latitude: Double, longitude: Double) {
         let mapView = view as! GMSMapView
 
@@ -54,6 +61,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
         
     }
     
+    /// Function that initializes the Google Map services object with an API key retieved from a local config file. A camera position is aplied and the MapViewController's view is set to an instance of the GMSMapView Class.
     func setupMap()  {
         GMSServices.provideAPIKey(self.getConfigDictionary()!["googleMapsAPIKey"] as! String)
         _camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
@@ -70,6 +78,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
 
     }
     
+    /// Function that sets up the Core Location CLLocationManager instance and configures it so that this app will receive updates about changes in the users location.
     func setupGPS()  {
         _locationManager.delegate = self
         _locationManager.distanceFilter = kCLDistanceFilterNone
@@ -90,13 +99,15 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
 
     }
     
+    /// Function that configures this View Controller's parenting Navigation Controller to display a title as well as creating and adding a 'Menu' button that the user may interact with.
     func setupNavigationController()  {
         title = "iCars Test"
-        let menuButton = UIBarButtonItem.init(title:"Menu", style:.plain, target: self, action:#selector(ViewController.menuButtonPressed))
+        let menuButton = UIBarButtonItem.init(title:"Menu", style:.plain, target: self, action:#selector(MapViewController.menuButtonPressed))
         navigationItem.rightBarButtonItem = menuButton
         
     }
     
+    /// Action triggered when the user presses the 'Menu' button located inside the navigation controller's navigation bar.
     func menuButtonPressed() {
        print("button pressed")
     }
