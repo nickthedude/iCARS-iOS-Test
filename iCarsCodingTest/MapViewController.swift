a75e8002dade4822f45f814675403c94d9a79357
nickthedude <nickthedude@me.com>
2016-10-30 01:32:39 -0700

    Brought over stylization methods from auto layout branch. Added functionality for NY and SF buttons.

--- iCarsCodingTest/MapViewController.swift
+++ iCarsCodingTest/MapViewController.swift
@@ -17,15 +17,22 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
     
     /// Google Maps camera object member variable used to manipulate the viewing bounds of a GMSMapView object
     private var _camera = GMSCameraPosition.camera(withLatitude: 0.0, longitude: 0.0, zoom: 6.0)
+    
+    private var _mapView: GMSMapView!
+
+    private var _shouldSuspendLocationUpdates  = false
 
     @IBOutlet private var _sideMenu: UIView!
     
     private var _sideMenuIsOnsceen  = false
+    
+    private var _markers : [GMSMarker] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGPS()
         setupMap()
+        stylizeSideMenu()
         adjustSideMenuForParentsCurrentFrame()
         
         
@@ -57,32 +64,22 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
     /// - Parameter latitude: a latitude in Double format used to orient the _camera along a vertical axis
     /// - Parameter longitude: a longitude in Double format used to orient the _camera along a horizontal axis
     func updateMapViewToLocation(latitude: Double, longitude: Double) {
-        let mapView = view as! GMSMapView
-
-        _camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: mapView.camera.zoom)
-        mapView.camera = _camera
         
+        _camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: _mapView.camera.zoom)
+        _mapView.camera = _camera
         
+ 
     }
     
     /// Function that initializes the Google Map services object with an API key retieved from a local config file(configuration.plist). A camera position is aplied and the MapViewController's view is set to an instance of the GMSMapView Class.
     func setupMap()  {
         GMSServices.provideAPIKey(self.getConfigDictionary()!["googleMapsAPIKey"] as! String)
         _camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: view.frame, camera: _camera)
-        mapView.isMyLocationEnabled = true
-        view = mapView
+        _mapView = GMSMapView.map(withFrame: view.frame, camera: _camera)
+        _mapView.isMyLocationEnabled = true
+        view = _mapView
         
         view.addSubview(_sideMenu)
-
-        
-//        // Creates a marker in the center of the map.
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
-
     }
     
     /// Function that sets up the Core Location CLLocationManager instance and configures it so that this app will receive updates about changes in the users location.
@@ -96,8 +93,9 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        updateMapViewToLocation(latitude: locations[0].coordinate.latitude , longitude: locations[0].coordinate.longitude)
-        print("location received")
+        if !_shouldSuspendLocationUpdates {
+            updateMapViewToLocation(latitude: locations[0].coordinate.latitude , longitude: locations[0].coordinate.longitude)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -111,7 +109,13 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
         title = "iCars Test"
         let menuButton = UIBarButtonItem.init(title:"Menu", style:.plain, target: self, action:#selector(MapViewController.menuButtonPressed))
         navigationItem.rightBarButtonItem = menuButton
-        
+        if let navFont = UIFont(name: "HelveticaNeue-Light", size: 17.0) {
+            let navBarAttributesDictionary: [String: AnyObject]? = [
+                NSForegroundColorAttributeName: UIColor.black,
+                NSFontAttributeName: navFont
+            ]
+            navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
+        }
     }
     
     /// Action triggered when the user presses the 'Menu' button located inside the navigation controller's navigation bar.
@@ -123,15 +127,31 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
     /// Adjusts a side menu View frame. This display of this side menu will be the response to a user tapping the 'Menu' button in the navigation bar.
     func adjustSideMenuForParentsCurrentFrame() {
        // let sideMenuView
-        _sideMenu.frame = CGRect(x: view.frame.width, y: (view.frame.height * 0.125), width: _sideMenu.frame.width, height: _sideMenu.frame.height)
+        _sideMenu.frame = CGRect(x: view.frame.width, y: (view.frame.height * 0.08), width: _sideMenu.frame.width, height: _sideMenu.frame.height)
 
     }
     
+    func stylizeSideMenu() {
+        //only apply the blur if transparency effects are enabled
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            _sideMenu.backgroundColor = UIColor.clear
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView.frame = _sideMenu.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            _sideMenu.insertSubview(blurEffectView, at: 0)
+            blurEffectView.clipsToBounds = true
+        }
+    }
+
+    
     func toggleSideMenuOnScreen() {
+        let distanceMultiplier : CGFloat = 0.6
         if _sideMenuIsOnsceen {
             // move sideMenu Off-screen
             UIView.beginAnimations(nil, context: nil)
-            _sideMenu.center = CGPoint(x: (_sideMenu.center.x + 200.0), y: _sideMenu.center.y)
+            _sideMenu.center = CGPoint(x: (_sideMenu.center.x + (view.frame.size.width * distanceMultiplier)), y: _sideMenu.center.y)
             UIView.commitAnimations()
             _sideMenuIsOnsceen = false
 
@@ -139,13 +159,43 @@ class MapViewController: UIViewController , CLLocationManagerDelegate {
         } else {
             // move sideMenu On-screen
             UIView.beginAnimations(nil, context: nil)
-            _sideMenu.center = CGPoint(x: (_sideMenu.center.x - 200.0), y: _sideMenu.center.y)
+            _sideMenu.center = CGPoint(x: (_sideMenu.center.x - (view.frame.size.width * distanceMultiplier)), y: _sideMenu.center.y)
             UIView.commitAnimations()
             _sideMenuIsOnsceen = true
 
         }
         
     }
+    @IBAction func newYorkButtonPressed(_ sender: AnyObject) {
+        toggleSideMenuOnScreen()
+        let latitude = 40.7128, longitude = -74.0059
+        _shouldSuspendLocationUpdates = true
+        placeMarkerOnMapfor(latitude: latitude, longitude: longitude, title: "New York City", snippet: "United States")
+        updateMapViewToLocation(latitude: latitude, longitude:longitude )
+    }
+    @IBAction func SFButtonPressed(_ sender: AnyObject) {
+        //37.7749° N, 122.4194° W
+        toggleSideMenuOnScreen()
+        let latitude = 37.7749, longitude = -122.4194
+        _shouldSuspendLocationUpdates = true
+        placeMarkerOnMapfor(latitude: latitude, longitude: longitude, title: "San Francisco", snippet: "United States")
+        updateMapViewToLocation(latitude: latitude, longitude:longitude )
+    }
+    @IBAction func SFToNYButtonPressed(_ sender: AnyObject) {
+    }
+    
+    func placeMarkerOnMapfor(latitude: Double, longitude: Double, title: String?, snippet: String?) {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude:latitude, longitude: longitude)
+        if let titleString = title {
+            marker.title = titleString
+        }
+        if let snippetString = snippet {
+            marker.snippet = snippetString
+        }
+        marker.map = _mapView
+    }
+    
     
 }
 
